8bbfe79e7f826674c8d17de82c57f6cca1289905
Lordaniket <42113553+Lordaniket@users.noreply.github.com>
2025-04-30 19:31:28 +0000

    IF Statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+// if
+
+if (true){
+    // EXECUTED
+}
+
+if (false){
+    // NOT EXECUTED
+}
+
+// Operators : <, >, <=, >=, ==, !=, ===. !==
+
+//////////////////////////////////////////
+
+
+if(true)
+    console.log("Scope exists upto one line by default");
+    
